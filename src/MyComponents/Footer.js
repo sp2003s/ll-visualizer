87d1326525d54531ba2f408fc57010ff34996893
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-06-07 23:49:07 +0530

    footer link

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className='container-fluid footer'>
 
         <div className='row d-flex justify-content-around'> 
-            <div className='col-md-6'> <img src='/images/linkedIN512.png'/> </div>
-            <div className='col-md-6'> <img src='/images/github512.png'/> </div>
+            <div className='col-md-6'> <a href='https://www.linkedin.com/in/shaurya-pratap-singh-/' target="blank" rel="noopener noreferrer">  <img src='/images/linkedIN512.png'/> </a> </div>
+            <div className='col-md-6'> <a href='https://github.com/sp2003s' target="blank" rel="noopener noreferrer"> <img src='/images/github512.png'/> </a> </div>
         </div>
 
     </div>
