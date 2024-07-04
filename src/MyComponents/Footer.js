d85c3f0048e13b0aa9f7f2c8498e002fd801a8dc
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-04 19:26:24 +0530

    Footer message

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -9,6 +9,10 @@ const Footer = () => {
             <div className='col-md-6'> <a href='https://github.com/sp2003s' target="blank" rel="noopener noreferrer"> <img src='/images/github512.png'/> </a> </div>
         </div>
 
+        <div className='row d-flex justify-content-around'> 
+            <div className = 'col-md-12'>Made With <b style={{color: 'red', fontSize: '1.6em'}}>&hearts;</b> By Shaurya Pratap Singh</div>
+        </div>
+
     </div>
   )
 }
